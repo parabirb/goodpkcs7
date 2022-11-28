b08ccf8ac4df1c065d409113816552f24d557343
parabirb <parabirb@protonmail.ch>
2022-11-27 22:38:19 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# goodpkcs7
+a good library for pkcs7 padding
+
+## rationale
+there isn't a good pkcs7 library on NPM. pkcs7 is okay, but it only supports multiples of 16 bytes.
+
+## usage
+install with `npm install goodpkcs7`
+
+example usage:
+```js
+// get the pad and unpad functions
+const { pad, unpad } = require("goodpkcs7");
+// pad an array, buffer, or uint8array
+// note that the result will be a uint8array
+const padded = pad([1, 2, 3, 4]);
+// unpad a uint8array
+const unpadded = unpad(padded);
+```
+
+goodpkcs7 provides two methods:
+* `pad(message, bytes = 16)` - Pads an array, buffer, or Uint8Array message to a multiple of the specified number of bytes (default 16). Returns a Uint8Array.
+* `unpad(message, bytes = 16)` - Unpads an array, buffer, or Uint8Array message given that its length is a multiple of the specified number of bytes (default 16). Returns a Uint8Array. Errors if the message is not properly padded.
+
+goodpkcs7 can be used in the same way as pkcs7, but goodpkcs7 lets you specify the number of padding bytes if necessary.
+
+## licensing
+goodpkcs7 is in the public domain. do whatever you want with it. i don't care.
---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+    pkcs7
+    made by parabirb, 2022
+    public domain
+    meow
+*/
+
+// message padding function
+function pad(message, bytes = 16) {
+    // compute the nearest multiple of bytes
+    let nearest = Math.ceil(message.length / bytes) * bytes;
+    // compute the padding length
+    let paddingLength = message.length < nearest ? nearest - message.length : bytes;
+    // create a uint8array for padding
+    let padding = new Uint8Array(paddingLength);
+    // fill it
+    for (let i = 0; i < paddingLength; i++) padding[i] = paddingLength;
+    // return the padded message
+    return new Uint8Array([...message, ...padding]);
+}
+
+// message unpadding function
+function unpad(message, bytes = 16) {
+    // error if the message length isn't a multiple of bytes
+    if (message.length % bytes !== 0) throw new Error("Message length is not a multiple of bytes!");
+    // get the padding length
+    let paddingLength = message[message.length - 1];
+    // error if the padding length is greater than possible
+    if (paddingLength > bytes) throw new Error("Padding length is greater than the max number of padding bytes; are you sure this message is padded properly?");
+    // go through the array and error if the padding doesn't match
+    for (let i = message.length - 1; i >= message.length - paddingLength; i--) {
+        if (message[i] !== paddingLength) throw new Error("Message is not properly padded!");
+    }
+    // return the unpadded message
+    return message.slice(0, message.length - paddingLength);
+}
+
+// exports
+module.exports = {
+    pad,
+    unpad
+};
